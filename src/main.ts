bce9ae255adb8ec0f75fb697a71c7938b0f47567
kubilaysabah <kubilaysabah@gmail.com>
2024-01-13 17:20:18 +0300

    init prisma

--- src/main.ts
+++ src/main.ts
@@ -1,7 +1,25 @@
-import { NestFactory } from '@nestjs/core';
-import { AppModule } from './app.module';
+import { NestFactory } from "@nestjs/core";
+import { AppModule } from "./app.module";
+import { PrismaClient } from "@prisma/client";
 
-(async () => {
+const main = async (): Promise<void> => {
   const app = await NestFactory.create(AppModule);
   await app.listen(3000);
+};
+
+const initPrisma = async (): Promise<void> => {
+  const prisma = new PrismaClient();
+
+  try {
+    await prisma.$disconnect();
+  } catch (error) {
+    console.error(error);
+    await prisma.$disconnect();
+    process.exit(1);
+  }
+};
+
+(async (): Promise<void> => {
+  await main();
+  await initPrisma();
 })();
