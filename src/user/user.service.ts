51990e089cfc81cd6663004c276fb9f3cfb6c9ce
fh-kubilay <kubilaysabah@gmail.com>
2024-01-14 22:25:10 +0300

    add types user service

--- src/user/user.service.ts
+++ src/user/user.service.ts
@@ -1,12 +1,13 @@
 import { Injectable } from '@nestjs/common';
 import { PrismaService } from '../prisma.service'
+import { Users } from '@prisma/client'
 
 @Injectable()
 export class UserService {
 
   constructor(private prismaService: PrismaService) {}
 
-  findOne(email: string) {
+  findOne(email: string): Promise<Users> {
     return this.prismaService.users.findFirst({
       where: {
         email
