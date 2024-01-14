791b44b8f90ba6ecf5d9b659e37cfcdc7a1556bb
fh-kubilay <kubilaysabah@gmail.com>
2024-01-14 22:24:23 +0300

    add prefix to routes

--- src/main.ts
+++ src/main.ts
@@ -3,5 +3,6 @@ import { AppModule } from "./app.module";
 
 (async (): Promise<void> => {
   const app = await NestFactory.create(AppModule);
+  app.setGlobalPrefix("api");
   await app.listen(3000);
 })();
