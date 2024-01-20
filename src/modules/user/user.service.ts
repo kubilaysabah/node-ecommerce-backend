53c8b7c3f4e11df752898476d2caf1af00f1ffb8
kubilaysabah <kubilaysabah@gmail.com>
2024-01-20 08:37:34 +0300

    Create user.service.ts

---
+++ src/modules/user/user.service.ts
@@ -0,0 +1,53 @@
+import { Injectable } from '@nestjs/common';
+import { Users } from '@prisma/client'
+
+import { PrismaService } from '../../prisma.service'
+import { FindUserParams } from './dto/find-user.dto'
+import { UpdateUserDto } from './dto/update-user.dto'
+
+@Injectable()
+export class UserService {
+
+  constructor(private prismaService: PrismaService) {}
+
+  findOne({ email, id }: FindUserParams): Promise<Users> {
+    return this.prismaService.users.findFirst({
+      where: {
+        ...(id ? { id: +id } : { email })
+      }
+    })
+  }
+
+  findAll() {
+    return this.prismaService.users.findMany();
+  }
+
+  update(id:number, { role, email, image, lastname, phone, password, firstname }: UpdateUserDto) {
+    return this.prismaService.users.update({
+      where: {
+        id,
+      },
+      data: {
+        firstname,
+        lastname,
+        email,
+        phone,
+        password,
+        image,
+        role: {
+          connect: {
+            id: role
+          }
+        }
+      }
+    })
+  }
+
+  remove(id: number) {
+    return this.prismaService.users.delete({
+      where: {
+        id
+      }
+    })
+  }
+}
