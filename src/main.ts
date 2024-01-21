ca785950b7548afef944b8de98e46b85665493e3
fh-kubilay <kubilaysabah@gmail.com>
2024-01-21 12:21:38 +0300

    add log interceptor and not-found interceptor

--- src/main.ts
+++ src/main.ts
@@ -2,14 +2,18 @@ import { NestFactory } from "@nestjs/core";
 import { ValidationPipe } from "@nestjs/common";
 import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
 import { AppModule } from "./modules/app/app.module";
+import { LoggingInterceptor } from './interceptors/logging.interceptor'
+import { NotFoundInterceptor } from './interceptors/not-found.interceptor'
 
 (async (): Promise<void> => {
   const app = await NestFactory.create(AppModule);
   app.enableCors({
-    origin: "http://localhost:3000",
+    origin: "http://localhost:4200",
   })
   app.setGlobalPrefix("api");
   app.useGlobalPipes(new ValidationPipe());
+  app.useGlobalInterceptors(new LoggingInterceptor());
+  app.useGlobalInterceptors(new NotFoundInterceptor());
 
   const config = new DocumentBuilder()
       .setTitle('E-Commerce API')
