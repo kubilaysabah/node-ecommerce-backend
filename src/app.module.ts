4320e9813e7d7cf88f1b16ecaa638eb083383ec7
kubilaysabah <kubilaysabah@gmail.com>
2024-02-17 09:00:03 +0300

    update schema

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,17 +3,12 @@ import { DevtoolsModule } from '@nestjs/devtools-integration'
 
 import { SharedModule } from '@shared/shared.module'
 
-import { AuthModule } from '@modules/auth/auth.module'
-import { CustomerModule } from '@modules/customer/customer.module'
-
 @Module({
 	imports: [
 		DevtoolsModule.register({
 			http: process.env.NODE_ENV !== 'production',
 		}),
 		SharedModule,
-		AuthModule,
-		CustomerModule,
 	],
 	controllers: [],
 	providers: [],
