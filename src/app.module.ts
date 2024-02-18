257319ea2e95ad51964650191ddde8caf80fda05
Kubilay <kubilaysabah@gmail.com>
2024-02-18 14:58:37 +0300

    add customer module

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,6 +2,7 @@ import { Module } from '@nestjs/common'
 import { DevtoolsModule } from '@nestjs/devtools-integration'
 
 import { SharedModule } from '@shared/shared.module'
+import { CustomerModule } from '@modules/customer/customer.module'
 
 @Module({
 	imports: [
@@ -9,8 +10,10 @@ import { SharedModule } from '@shared/shared.module'
 			http: process.env.NODE_ENV !== 'production',
 		}),
 		SharedModule,
+		CustomerModule,
 	],
 	controllers: [],
 	providers: [],
 })
-export class AppModule {}
+export class AppModule {
+}
